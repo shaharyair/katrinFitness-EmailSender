a00eb4b763036c7f0e2486efa93c597a4838035a
Shahar Yair <shaharyair12@gmail.com>
2023-04-29 22:40:40 +0300

    seperated server and send-email

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
 const cors = require("cors");
-const fs = require("fs");
+
+const sendEmailRouter = require("./send-email"); // import the send-email router module
 
 require("dotenv").config();
 
@@ -16,37 +16,8 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname + "/public")));
 
-app.post("/send-email", (req, res) => {
-  const { fullName, phoneNumber } = req.body;
-
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "shaharyair12@gmail.com",
-      pass: process.env.GMAIL_PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: "shaharyair12@gmail.com",
-    to: "shaharyair12@gmail.com",
-    subject: "קאתרין יאיר מאמנת כושר - מתעניינת חדשה!",
-    html: fs
-      .readFileSync(__dirname + "/email-template.html", "utf-8")
-      .replace(/%fullName%/g, fullName)
-      .replace(/%phoneNumber%/g, phoneNumber),
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send("קרתה שגיאה בשליחת המייל.");
-    } else {
-      console.log("Email sent");
-      res.send("המייל נשלח בהצלחה!");
-    }
-  });
-});
+// use the sendEmailRouter for the /send-email endpoint
+app.use("/send-email", sendEmailRouter);
 
 const PORT = process.env.PORT || 3000;
 
